789493c9d26a3c386452b061eff6babd16836b9b
Kartik Mishra <kartikmishra.km44@gmail.com>
2019-07-13 17:05:11 +0530

    committing changes after generating components and configureing routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCPmPuJVibusbjSlgvuGBOTuBGFz6oqmoE",
+    authDomain: "food-shopping-cart.firebaseapp.com",
+    databaseURL: "https://food-shopping-cart.firebaseio.com",
+    projectId: "food-shopping-cart",
+    storageBucket: "",
+    messagingSenderId: "582398192042",
+    appId: "1:582398192042:web:e08752e5dddff8f6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCPmPuJVibusbjSlgvuGBOTuBGFz6oqmoE",
+    authDomain: "food-shopping-cart.firebaseapp.com",
+    databaseURL: "https://food-shopping-cart.firebaseio.com",
+    projectId: "food-shopping-cart",
+    storageBucket: "",
+    messagingSenderId: "582398192042",
+    appId: "1:582398192042:web:e08752e5dddff8f6"
+  }
 };
 
 /*
